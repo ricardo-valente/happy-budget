d8205d9d3a505c8ff31aaf7f5f52d0dbd1fe5d1c
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-11-09 22:10:59 +0000

    changed profile to serve command

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_http2.default.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('<h1>Ricardo Valente</h1>\n');
+}).listen(3000, 'localhost');
+
+console.log('Server running at http://localhost:3000/');
