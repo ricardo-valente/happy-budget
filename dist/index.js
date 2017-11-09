d1555afb5fbb2018c05a16e491c8e04e2cb8a4d7
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-11-09 22:21:48 +0000

    Changed to https protocol

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var _http = require('http');
+var _https = require('https');
 
-var _http2 = _interopRequireDefault(_http);
+var _https2 = _interopRequireDefault(_https);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_http2.default.createServer(function (req, res) {
+_https2.default.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('<h1>Ricardo Valente</h1>\n');
 }).listen(1337, '127.0.0.1');
