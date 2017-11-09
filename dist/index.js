aaa3bfee7687feab4da853f524040a2decd428d5
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-11-09 22:51:03 +0000

    changed port listening function

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var _https = require('https');
+var _http = require('http');
 
-var _https2 = _interopRequireDefault(_https);
+var _http2 = _interopRequireDefault(_http);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_https2.default.createServer(function (req, res) {
+_http2.default.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('<h1>Ricardo Valente</h1>\n');
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT || 5000);
 
 console.log('Server running at http://localhost:3000/');
