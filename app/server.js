33b02e9ee288d7b5927e8f266f9ed425591d1d3c
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-11-16 10:31:18 +0000

    server changed to express

--- app/server.js
+++ app/server.js
@@ -14,63 +14,15 @@ var _path2 = _interopRequireDefault(_path);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var port = 6000;
-var cssDir = __dirname + '/css/';
-var jsDir = __dirname + '/js/';
-var viewsDir = __dirname + '/views/';
+var express = require('express');
 
-function filePath(request) {
-  var filePath = void 0;
-  if (request.url === '/') {
-    filePath = 'index.html';
-  } else {
-    filePath = request.url.replace('/', '');
-  }
-  return _path2.default.join(__dirname, String(filePath));
-}
+var app = express();
+var port = 3000;
 
-function contentType(filePath) {
-  var extname = String(_path2.default.extname(filePath)).toLowerCase();
-  var contentType = 'text/html';
-  var mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.svg': 'application/image/svg+xml'
-  };
-  contentType = mimeTypes[extname] || 'application/octet-stream';
-  return contentType;
-}
+app.get('/', function (req, res) {
+  return res.send('Hello World!');
+});
 
-function getFileData(request, response) {
-  _fs2.default.readFile(filePath(request), 'utf8', function (error, data) {
-    if (error) {
-      if (error.code == 'ENOENT') {
-        _fs2.default.readFile('./404.html', function (error, data) {
-          response.writeHead(200, { 'Content-Type': contentType(filePath(request)) });
-          response.end(data, 'utf-8');
-        });
-      } else {
-        response.writeHead(500);
-        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-        response.end();
-      }
-    } else {
-      response.writeHead(200, { 'Content-Type': contentType(filePath(request)) });
-      response.write(data);
-    }
-    response.end();
-  });
-}
-
-_http2.default.createServer(getFileData).listen(process.env.PORT || port);
+app.listen(port, function () {
+  return console.log('Example app listening on port 5000!');
+});
--- dev/server.js
+++ dev/server.js
@@ -1,65 +1,11 @@
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
+import http from 'http'
+import fs from 'fs'
+import path from 'path'
+const express = require('express')
 
-const port = 6000;
-const cssDir = __dirname + '/css/';
-const jsDir = __dirname + '/js/';
-const viewsDir = __dirname + '/views/';
+const app = express()
+const port = 3000
 
-function filePath(request) {
-  let filePath;
-  if (request.url === '/') {
-    filePath = 'index.html';
-  } else {
-    filePath = request.url.replace('/', '');
-  }
-  return path.join(__dirname, String(filePath));
-}
+app.get('/', (req, res) => res.send('Hello World!'))
 
-function contentType(filePath) {
-  let extname = String(path.extname(filePath)).toLowerCase();
-  let contentType = 'text/html';
-  const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.svg': 'application/image/svg+xml'
-  };
-  contentType = mimeTypes[extname] || 'application/octet-stream';
-  return contentType;
-}
-
-function getFileData(request, response) {
-  fs.readFile(filePath(request), 'utf8', (error, data) => {
-    if(error) {
-      if(error.code == 'ENOENT'){
-        fs.readFile('./404.html', function(error, data) {
-          response.writeHead(200, { 'Content-Type': contentType(filePath(request)) });
-          response.end(data, 'utf-8');
-        });
-      }
-      else {
-        response.writeHead(500);
-        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-        response.end();
-      }
-    } else {
-      response.writeHead(200, {'Content-Type': contentType(filePath(request))});
-      response.write(data);
-    }
-    response.end();
-  });
-}
-
-http.createServer(getFileData).listen(process.env.PORT || port);
+app.listen(port, () => console.log('Example app listening on port 5000!'))
