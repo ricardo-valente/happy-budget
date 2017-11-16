cc7252f54b001fbfcb7adba965c248419bd5def9
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-11-16 16:47:25 +0000

    Express server port changes

--- app/server.js
+++ app/server.js
@@ -15,14 +15,17 @@ var _path2 = _interopRequireDefault(_path);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var express = require('express');
+// const dotenv = require('dotenv').config({path: path.join(process.env.PWD, '.env')})
 
 var app = express();
 var port = 3000;
+// const env = process.env.NODE_ENV
 
 app.get('/', function (req, res) {
   return res.send('Hello World!');
 });
 
-app.listen(port, function () {
+app.listen(process.env.PORT || port, function () {
   return console.log('Example app listening on port 5000!');
 });
+// console.log(env)
--- dev/server.js
+++ dev/server.js
@@ -2,10 +2,13 @@ import http from 'http'
 import fs from 'fs'
 import path from 'path'
 const express = require('express')
+// const dotenv = require('dotenv').config({path: path.join(process.env.PWD, '.env')})
 
 const app = express()
 const port = 3000
+// const env = process.env.NODE_ENV
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log('Example app listening on port 5000!'))
+app.listen(process.env.PORT || port, () => console.log('Example app listening on port 5000!'))
+// console.log(env)
