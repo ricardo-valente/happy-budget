6e3843615e3e4efa9dcaf415b673da7a31bf8e56
Ricardo Valente <ricardo.valente@talkdesk.com>
2017-11-30 10:12:02 +0000

    Added budget route

---
+++ public/routes/budget.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _config = require('../../config');
+
+var _config2 = _interopRequireDefault(_config);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var router = _express2.default.Router();
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+module.exports = router;
